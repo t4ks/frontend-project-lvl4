9d47472991f316462562b9854268d43b2a4436c1
Pavel Sirotkin <pav.pnz@gmail.com>
2021-11-13 22:46:17 +0100

    fixes

--- src/index.jsx
+++ src/index.jsx
@@ -1,3 +1,15 @@
-import runApp from './init.jsx';
+import React from 'react';
+import { render } from 'react-dom';
 
-runApp();
+import { Provider as RollBarProvider, ErrorBoundary, LEVEL_WARN } from '@rollbar/react';
+import InitializedApp from './init.jsx';
+import { rollbarConfig } from './rollbar';
+
+render(
+  <RollBarProvider config={rollbarConfig}>
+    <ErrorBoundary level={LEVEL_WARN}>
+      <InitializedApp />
+    </ErrorBoundary>
+  </RollBarProvider>,
+  document.getElementById('chat'),
+);
--- src/init.jsx
+++ src/init.jsx
@@ -6,23 +6,16 @@ import 'regenerator-runtime/runtime.js';
 import '../assets/application.scss';
 
 import React from 'react';
-import { render } from 'react-dom';
-
-import { ErrorBoundary, LEVEL_WARN, Provider as RollBarProvider } from '@rollbar/react';
 import { Provider } from 'react-redux';
 import App from './components/App.jsx';
 import store from './store';
-import { rollbarConfig } from './rollbar';
 /* eslint-disable-next-line no-unused-vars */
 import i18n from './i18n';
 
-export default () => render(
+const InitializedApp = () => (
   <Provider store={store}>
-    <RollBarProvider config={rollbarConfig}>
-      <ErrorBoundary level={LEVEL_WARN}>
-        <App />
-      </ErrorBoundary>
-    </RollBarProvider>
-  </Provider>,
-  document.getElementById('chat'),
+    <App />
+  </Provider>
 );
+
+export default InitializedApp;
